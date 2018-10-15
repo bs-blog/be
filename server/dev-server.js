98ac5c8f3aba8f7e4b33014136b192eb9ca3214c
waheng chang <wahengchang@gmail.com>
2018-10-15 22:38:09 +0800

    reset cache when update, create, delete data

--- server/dev-server.js
+++ server/dev-server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const uploadRouter = require('./routers/upload')
 
 const app = express();
 const port = process.env.PORT || 9009;
 
-app.use(uploadRouter)
+app.use(require('./routers/system'))
+app.use(require('./routers/upload'))
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
